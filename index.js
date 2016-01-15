59b5254f24007d2b2e999630087c55bc55e0dae6
zodern <zodern@users.noreply.github.com>
2016-01-14 21:30:17 -0600

    Find file systems provided by apps

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+var fileSystems = [];
+
+// Get a list of apps that add a file system
+// This is used in the file picker.
+Silk.api.listen('apps/state', {}, function (e, d) {
+  console.log('called', d.length);
+  d.forEach(function (item) {
+    if(item.fileSystem) {
+      item.fileSystem.appId = item.id;
+      fileSystems.push(item.fileSystem);
+    }
+  });
+});
+
 Silk.methods.add({
-	'sleek/apps': function (data, call_obj, send) {
-		return Silk.api.listen('apps/state', {}, function (err, data) {
-			send(null, data);
-		});
-	}
+  /* Sends a list of the apps with their state */
+  'sleek/apps': function (data, call_obj, send) {
+    return Silk.api.listen('apps/state', {}, function (err, data) {
+      send(null, data);
+    });
+  }
 });
